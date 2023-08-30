fcf7c0440dbd26bf69fc3c2a62d5024368a3b881
Alexandre Dantas <alexandredantas690@outlook.com>
2023-08-29 22:32:49 -0300

    Add jogo da forca em C#

---
+++ C#/JogoDaForca/ForcaDoKayneWest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ForcaDoKayneWest {
+    class JogoDaForca {
+        static bool menu() {
+            Console.Write("Digite:\n1 - Para Jogar\n2 - Para sair\nResposta: ");
+            int resposta = int.Parse(Console.ReadLine());
+            return resposta == 1;
+        }
+        static async Task Main(string[] args) {
+            Console.WriteLine("Esse Jogo da forca é feito com as frases do Kayne West!");
+            Console.WriteLine("Vamos te dar a primeira palavra de uma frase\nSeu objetivo é acertar a palavra com até 10 tentativas!");
+            Console.WriteLine("Observação: As frases ainda estão em inglês!");
+            HttpClient httpClient = new HttpClient();
+            string apiUrl = "https://api.kanye.rest/text";
+            while (menu()) {
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode) {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    string[] word = responseBody.Split(' ');
+                    Console.WriteLine("A palavra contém {0} letras", word[0].Length);
+                    Console.WriteLine(responseBody);
+                }
+            }
+            httpClient.Dispose();
+        }
+    }
+}

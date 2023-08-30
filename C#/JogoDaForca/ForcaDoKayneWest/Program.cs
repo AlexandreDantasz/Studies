844254f22b581929cdd7e771f1e8bf30cf5241bf
Alexandre Dantas <alexandredantas690@outlook.com>
2023-08-30 09:15:41 -0300

    Código em desenvolvimento

--- C#/JogoDaForca/ForcaDoKayneWest/Program.cs
+++ C#/JogoDaForca/ForcaDoKayneWest/Program.cs
@@ -2,6 +2,7 @@
     Autor: Alexandre Dantas de Mendonça
     Data: 29/08/2023
     Objetivo: Criar um jogo da forca, que utiliza palavras das frases (pérolas) do Kayne West, para rodar no Console.
+    Observações: Fazer uma "interface gráfica" para as palavras e um contador de tentativas.
 */
 
 using System;
@@ -14,6 +15,18 @@ namespace ForcaDoKayneWest {
             int resposta = int.Parse(Console.ReadLine());
             return resposta == 1;
         }
+
+        static int theBigOne(string[] search) {
+            int index = 0, size = search.Length, best = search[0].Length;
+            for (int i = 0; i < size; i++) {
+                if (search[i].Length > best) {
+                    index = i;
+                    best = search[i].Length;
+                } 
+            }
+            return index;
+        }
+
         static async Task Main(string[] args) {
             Console.WriteLine("Esse Jogo da forca é feito com as frases do Kayne West!");
             Console.WriteLine("Vamos te dar a primeira palavra de uma frase\nSeu objetivo é acertar a palavra com até 10 tentativas!");
@@ -24,8 +37,17 @@ namespace ForcaDoKayneWest {
                 HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
                 if (response.IsSuccessStatusCode) {
                     string responseBody = await response.Content.ReadAsStringAsync();
-                    string[] word = responseBody.Split(' ');
-                    Console.WriteLine("A palavra contém {0} letras", word[0].Length);
+                    string [] dictionary = responseBody.Split(' ');
+                    int wordIndex = theBigOne(dictionary);
+                    Console.WriteLine("A palavra contém {0} letras", dictionary[wordIndex].Length);
+                    string WordInterface = "";
+                    for (int i = 0; i < dictionary[wordIndex].Length; i++) WordInterface += '_';
+                    int tentativas = 0;
+                    while (tentativas != -1 && tentativas < 10) {
+                        Console.WriteLine("Tentativas: {0}", tentativas);
+                        Console.WriteLine("Palavra: {0}", WordInterface);
+                        Console.Write("Digite a letra: ");
+                    }
                     Console.WriteLine(responseBody);
                 }
             }
